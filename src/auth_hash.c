mgumz/alock

commit 3ce90ef69cf517f71952027587fc403ed4ba6276

Structures rewrite and initialization refactoring
Alock lacks one important feature - reinitialization upon the resolution
change of used display. Unfortunately with current application design it
was almost impossible to incorporate required changes - everything would
become one big entanglement mess.

The main purpose of this commit (and few commits before it) is to refactor
internal structures to allow "on the fly" modifications (reinitialization).
This commit also makes some simplifications to the initialization mechanism -
mainly for the maintenance and readability improvement.

src/auth_hash.c
@@ -15,8 +15,8 @@
 #include "../config.h"
 #endif
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <gcrypt.h>
@@ -33,9 +33,13 @@ static int algorithms[] = {
     GCRY_MD_SHA512,
     GCRY_MD_WHIRLPOOL,
 };
-static int selected_algorithm = 0;
-static int selected_digest_len;
-static unsigned char *user_digest = NULL;
+
+static struct moduleData {
+    int selected_algorithm;
+    int selected_digest_len;
+    unsigned char *user_digest;
+    char *user_hash;
+} data = { 0 };
 
 
 static unsigned char *hex2mem(unsigned char *mem, const char *str, int len) {
@@ -77,7 +81,7 @@ static char *mem2hex(char *str, const unsigned char *mem, int len) {
 }
 #endif
 
-static void alock_auth_hash_list(void) {
+static void module_cmd_list(void) {
 
     printf("list of available hashing algorithms:\n");
 
@@ -88,125 +92,125 @@ static void alock_auth_hash_list(void) {
 
 }
 
-static int alock_auth_hash_init(const char *args) {
+static void module_loadargs(const char *args) {
 
-    char *user_hash = NULL;
+    if (!args || strstr(args, "hash:") != args)
+        return;
+
+    char *arguments = strdup(&args[5]);
+    char *arg;
+    char *tmp;
+
+    for (tmp = arguments; tmp; ) {
+        arg = strsep(&tmp, ",");
+        if (strcmp(arg, "list") == 0) {
+            module_cmd_list();
+            exit(EXIT_SUCCESS);
+        }
+        if (strstr(arg, "type=") == arg) {
+            data.selected_algorithm = gcry_md_map_name(&arg[5]);
+        }
+        else if (strstr(arg, "hash=") == arg) {
+            free(data.user_hash);
+            data.user_hash = strdup(&arg[5]);
+        }
+        else if (strstr(arg, "file=") == arg) {
+            FILE *f;
+            int len;
+
+            if ((f = fopen(&arg[5], "r")) == NULL) {
+                perror("[hash]: unable to read file");
+                goto return_error;
+            }
+
+            /* allocate enough memory to contain all kind of hashes */
+            free(data.user_hash);
+            data.user_hash = (char *)malloc(HASH_DIGEST_MAX_LEN * 2 + 1);
+
+            fgets(data.user_hash, HASH_DIGEST_MAX_LEN * 2 + 1, f);
+            fclose(f);
+
+            /* strip trailing new line character, if any */
+            len = strlen(data.user_hash);
+            if (len > 0 && data.user_hash[len - 1] == '\n')
+                data.user_hash[len - 1] = '\0';
+        }
+    }
+
+return_error:
+    free(arguments);
+}
+
+static int module_init(struct aDisplayInfo *dinfo) {
+
     int status = 1;
     int len;
 
-    if (args && strstr(args, "hash:") == args) {
-        char *arguments = strdup(&args[5]);
-        char *arg;
-        char *tmp;
-        for (tmp = arguments; tmp; ) {
-            arg = strsep(&tmp, ",");
-            if (strcmp(arg, "list") == 0) {
-                alock_auth_hash_list();
-                exit(EXIT_SUCCESS);
-            }
-            if (strstr(arg, "type=") == arg) {
-                selected_algorithm = gcry_md_map_name(&arg[5]);
-            }
-            else if (strstr(arg, "hash=") == arg) {
-                free(user_hash);
-                user_hash = strdup(&arg[5]);
-            }
-            else if (strstr(arg, "file=") == arg) {
-                FILE *f;
-                int len;
-
-                if ((f = fopen(&arg[5], "r")) == NULL) {
-                    perror("[hash]: unable to read file");
-                    free(arguments);
-                    goto return_error;
-                }
-
-                /* allocate enough memory to contain all kind of hashes */
-                free(user_hash);
-                user_hash = (char*)malloc(HASH_DIGEST_MAX_LEN * 2 + 1);
-
-                fgets(user_hash, HASH_DIGEST_MAX_LEN * 2 + 1, f);
-                fclose(f);
-
-                /* strip trailing new line character, if any */
-                len = strlen(user_hash);
-                if (len > 0 && user_hash[len - 1] == '\n')
-                    user_hash[len - 1] = '\0';
-            }
-        }
-        free(arguments);
-    }
-
-    if (selected_algorithm == 0) {
+    if (data.selected_algorithm == 0) {
         fprintf(stderr, "[hash]: invalid or not specified type\n");
-        goto return_error;
+        return -1;
     }
 
-    if (user_hash == NULL) {
+    if (data.user_hash == NULL) {
         fprintf(stderr, "[hash]: not specified hash nor file\n");
-        goto return_error;
+        return -1;
     }
 
     /* initialize gcrypt subsystem */
     gcry_check_version(NULL);
 
-    selected_digest_len = gcry_md_get_algo_dlen(selected_algorithm);
-    len = strlen(user_hash);
+    data.selected_digest_len = gcry_md_get_algo_dlen(data.selected_algorithm);
+    len = strlen(data.user_hash);
 
-    if (selected_digest_len * 2 > len) {
+    if (data.selected_digest_len * 2 > len) {
         fprintf(stderr, "[hash]: incorrect hash for given type\n");
-        goto return_error;
+        return -1;
     }
 
-    user_digest = (unsigned char*)malloc(selected_digest_len);
-    hex2mem(user_digest, user_hash, selected_digest_len * 2);
+    data.user_digest = (unsigned char *)malloc(data.selected_digest_len);
+    hex2mem(data.user_digest, data.user_hash, data.selected_digest_len * 2);
 
-    goto return_success;
-
-return_error:
-    status = 0;
-    selected_algorithm = 0;
-
-return_success:
-    free(user_hash);
-    return status;
+    return 0;
 }
 
-static int alock_auth_hash_deinit() {
-    free(user_digest);
-    return 1;
+static void module_free(void) {
+    free(data.user_digest);
+    data.user_digest = NULL;
+    free(data.user_hash);
+    data.user_hash = NULL;
 }
 
-static int alock_auth_hash_auth(const char *pass) {
+static int module_authenticate(const char *pass) {
+
+    if (data.selected_algorithm == 0)
+        return -1;
 
     unsigned char *digest;
-    int status;
 
-    if (selected_algorithm == 0)
-        return 0;
-
-    digest = (unsigned char*)malloc(selected_digest_len);
-    gcry_md_hash_buffer(selected_algorithm, digest, pass, strlen(pass));
+    digest = (unsigned char *)malloc(data.selected_digest_len);
+    gcry_md_hash_buffer(data.selected_algorithm, digest, pass, strlen(pass));
 
 #if DEBUG
     char user_hash[HASH_DIGEST_MAX_LEN * 2 + 1];
     char test_hash[HASH_DIGEST_MAX_LEN * 2 + 1];
-    mem2hex(user_hash, user_digest, selected_digest_len);
-    mem2hex(test_hash, digest, selected_digest_len);
-    debug("user hash: %s", user_hash);
+    mem2hex(user_hash, data.user_digest, data.selected_digest_len);
+    mem2hex(test_hash, digest, data.selected_digest_len);
+    debug("user hash: %s", data.user_hash);
     debug("test hash: %s", test_hash);
 #endif
 
-    status = !memcmp(user_digest, digest, selected_digest_len);
+    int status = memcmp(data.user_digest, digest, data.selected_digest_len);
     free(digest);
 
     return status;
 }
 
 
-struct aAuth alock_auth_hash = {
-    "hash",
-    alock_auth_hash_init,
-    alock_auth_hash_deinit,
-    alock_auth_hash_auth,
+struct aModuleAuth alock_auth_hash = {
+    { "hash",
+        module_dummy_loadargs,
+        module_init,
+        module_dummy_free,
+    },
+    module_authenticate,
 };

src/alock.h
@@ -23,26 +23,22 @@
 #endif
 
 
-struct aXInfo {
+/* stores snapshot of screen intrinsic properties */
+struct aScreenInfo {
+    Colormap colormap;
+    Window root;
+    int width;
+    int height;
+};
+
+/* stores snapshot of display intrinsic properties */
+struct aDisplayInfo {
     Display *display;
-    Atom pid_atom;
-
-    int screens;
-    Window *root;
-    Colormap *colormap;
-    Window *window;
-    Cursor *cursor;
-    int *root_width;
-    int *root_height;
+    struct aScreenInfo *screens;
+    int screen_nb;
 };
 
-struct aAuth {
-    const char *name;
-    int (*init)(const char *args);
-    int (*deinit)();
-    int (*auth)(const char *pass);
-};
-
+/* possible states of the input module */
 enum aInputState {
     AINPUT_STATE_NONE,
     AINPUT_STATE_INIT,
@@ -50,74 +46,87 @@ enum aInputState {
     AINPUT_STATE_VALID,
     AINPUT_STATE_ERROR,
 };
-struct aInput {
+
+/* module base interface */
+struct aModule {
     const char *name;
-    int (*init)(const char *args, struct aXInfo *xinfo);
-    int (*deinit)(struct aXInfo *xinfo);
+    void (*loadargs)(const char *args);
+    int (*init)(struct aDisplayInfo *dinfo);
+    void (*free)();
+};
+
+struct aModuleAuth {
+    struct aModule m;
+    int (*authenticate)(const char *pass);
+};
+
+struct aModuleBackground {
+    struct aModule m;
+    Window (*getwindow)(int screen);
+};
+
+struct aModuleCursor {
+    struct aModule m;
+    Cursor (*getcursor)(void);
+};
+
+struct aModuleInput {
+    struct aModule m;
+    Window (*getwindow)(int screen);
+    KeySym (*keypress)(KeySym key);
     void (*setstate)(enum aInputState state);
-    KeySym (*keypress)(KeySym ks);
 };
 
-struct aCursor {
-    const char *name;
-    int (*init)(const char *args, struct aXInfo *xinfo);
-    int (*deinit)(struct aXInfo *xinfo);
-};
-
-struct aBackground {
-    const char *name;
-    int (*init)(const char *args, struct aXInfo *xinfo);
-    int (*deinit)(struct aXInfo *xinfo);
-};
-
-struct aOpts {
-    struct aAuth *auth;
-    struct aInput *input;
-    struct aCursor *cursor;
-    struct aBackground *background;
+/* module container structure */
+struct aModules {
+    struct aModuleAuth *auth;
+    struct aModuleBackground *background;
+    struct aModuleCursor *cursor;
+    struct aModuleInput *input;
 };
 
 
 /* authentication modules */
-extern struct aAuth alock_auth_none;
+extern struct aModuleAuth alock_auth_none;
 #if ENABLE_HASH
-extern struct aAuth alock_auth_hash;
+extern struct aModuleAuth alock_auth_hash;
 #endif
 #if ENABLE_PASSWD
-extern struct aAuth alock_auth_passwd;
+extern struct aModuleAuth alock_auth_passwd;
 #endif
 #if ENABLE_PAM
-extern struct aAuth alock_auth_pam;
+extern struct aModuleAuth alock_auth_pam;
 #endif
 
 /* background modules */
-extern struct aBackground alock_bg_none;
-extern struct aBackground alock_bg_blank;
+extern struct aModuleBackground alock_bg_none;
+extern struct aModuleBackground alock_bg_blank;
 #if ENABLE_IMLIB2
-extern struct aBackground alock_bg_image;
+extern struct aModuleBackground alock_bg_image;
 #endif
 #if ENABLE_XRENDER
-extern struct aBackground alock_bg_shade;
+extern struct aModuleBackground alock_bg_shade;
 #endif
 
 /* cursor modules */
-extern struct aCursor alock_cursor_none;
-extern struct aCursor alock_cursor_glyph;
+extern struct aModuleCursor alock_cursor_none;
+extern struct aModuleCursor alock_cursor_glyph;
 #if ENABLE_XCURSOR
-extern struct aCursor alock_cursor_xcursor;
+extern struct aModuleCursor alock_cursor_xcursor;
 #if (ENABLE_XRENDER && (ENABLE_XPM || ENABLE_IMLIB2))
-extern struct aCursor alock_cursor_image;
+extern struct aModuleCursor alock_cursor_image;
 #endif
 #endif
 
 /* input modules */
-extern struct aInput alock_input_none;
-extern struct aInput alock_input_frame;
+extern struct aModuleInput alock_input_none;
+extern struct aModuleInput alock_input_frame;
 
 
 /* dummy functions for module interface */
-int module_dummy_init(const char *args, struct aXInfo *xinfo);
-int module_dummy_deinit(struct aXInfo *xinfo);
+void module_dummy_loadargs(const char *args);
+int module_dummy_init(struct aDisplayInfo *dinfo);
+void module_dummy_free(void);
 
 
 /* helper functions defined in utils.c */

src/auth_none.c
@@ -1,6 +1,7 @@
 /*
  * alock - auth_none.c
  * Copyright (c) 2005 - 2007 Mathias Gumz <akira at fluxbox dot org>
+ *               2014 Arkadiusz Bokowy
  *
  * This file is a part of an alock.
  *
@@ -11,18 +12,17 @@
 #include "alock.h"
 
 
-static int alock_auth_none_init(const char *args) {
-    return 1;
-}
-
-static int alock_auth_none_auth(const char *pass) {
-    return 1;
+static int module_authenticate(const char *pass) {
+    (void)pass;
+    return 0;
 }
 
 
-struct aAuth alock_auth_none = {
-    "none",
-    alock_auth_none_init,
-    module_dummy_deinit,
-    alock_auth_none_auth,
+struct aModuleAuth alock_auth_none = {
+    { "none",
+        module_dummy_loadargs,
+        module_dummy_init,
+        module_dummy_free,
+    },
+    module_authenticate,
 };

src/auth_pam.c
@@ -60,24 +60,25 @@ static int alock_auth_pam_conv(int num_msg,
     return PAM_SUCCESS;
 }
 
-static int alock_auth_pam_init(const char *args) {
+static int module_init(struct aDisplayInfo *dinfo) {
+    (void)dinfo;
 
     struct passwd *pwd;
 
     errno = 0;
     if (!(pwd = getpwuid(getuid()))) {
         perror("[pam]: password entry for uid not found");
-        return 0;
+        return -1;
     }
 
     username = pwd->pw_name;
-    return 1;
+    return 0;
 }
 
-static int alock_auth_pam_auth(const char *pass) {
+static int module_authenticate(const char *pass) {
 
     if (!username)
-        return 0;
+        return -1;
 
     pam_handle_t *pam_handle = NULL;
     struct pam_conv conv = {
@@ -94,13 +95,15 @@ static int alock_auth_pam_auth(const char *pass) {
         retval = pam_authenticate(pam_handle, 0);
 
     pam_end(pam_handle, retval);
-    return retval == PAM_SUCCESS;
+    return !(retval == PAM_SUCCESS);
 }
 
 
-struct aAuth alock_auth_pam = {
-    "pam",
-    alock_auth_pam_init,
-    module_dummy_deinit,
-    alock_auth_pam_auth,
+struct aModuleAuth alock_auth_pam = {
+    { "pam",
+        module_dummy_loadargs,
+        module_init,
+        module_dummy_free,
+    },
+    module_authenticate,
 };

src/auth_passwd.c
@@ -36,13 +36,13 @@
 static struct passwd *pwd_entry = NULL;
 
 
-static int alock_auth_passwd_init(const char *args) {
+static int module_init(struct aDisplayInfo *dinfo) {
 
     errno = 0;
     pwd_entry = getpwuid(getuid());
     if (!pwd_entry) {
         perror("[passwd]: password entry for uid not found");
-        return 0;
+        return -1;
     }
 
 #if __linux && HAVE_SHADOW_H
@@ -59,26 +59,28 @@ static int alock_auth_passwd_init(const char *args) {
     if (strlen(pwd_entry->pw_passwd) < 13) {
         perror("[passwd]: password entry has no pwd");
         pwd_entry = NULL;
-        return 0;
+        return -1;
     }
 
-    return 1;
+    return 0;
 }
 
-static int alock_auth_passwd_auth(const char *pass) {
+static int module_authenticate(const char *pass) {
 
     if (pass == NULL || pwd_entry == NULL)
-        return 0;
+        return -1;
 
     /* simpler, and should work with crypt() algorithms using longer
        salt strings (like the md5-based one on freebsd).  --marekm */
-    return !strcmp(crypt(pass, pwd_entry->pw_passwd), pwd_entry->pw_passwd);
+    return strcmp(crypt(pass, pwd_entry->pw_passwd), pwd_entry->pw_passwd);
 }
 
 
-struct aAuth alock_auth_passwd = {
-    "passwd",
-    alock_auth_passwd_init,
-    module_dummy_deinit,
-    alock_auth_passwd_auth,
+struct aModuleAuth alock_auth_passwd = {
+    { "passwd",
+        module_dummy_loadargs,
+        module_init,
+        module_dummy_free,
+    },
+    module_authenticate,
 };

src/bg_blank.c
@@ -18,77 +18,94 @@
 #include "alock.h"
 
 
-static Window *window = NULL;
+static struct moduleData {
+    struct aDisplayInfo *dinfo;
+    Window *windows;
+    char *colorname;
+} data = { 0 };
 
 
-static int alock_bg_blank_init(const char *args, struct aXInfo *xinfo) {
+static void module_loadargs(const char *args) {
 
-    if (!xinfo)
-        return 0;
+    if (!args || strstr(args, "blank:") != args)
+        return;
 
-    Display *dpy = xinfo->display;
-    char *color_name = NULL;
-    int scr;
+    char *arguments = strdup(&args[6]);
+    char *arg;
+    char *tmp;
 
-    if (args && strstr(args, "blank:") == args) {
-        char *arguments = strdup(&args[6]);
-        char *arg;
-        char *tmp;
-        for (tmp = arguments; tmp; ) {
-            arg = strsep(&tmp, ",");
-            if (strstr(arg, "color=") == arg) {
-                free(color_name);
-                color_name = strdup(&arg[6]);
-            }
+    for (tmp = arguments; tmp; ) {
+        arg = strsep(&tmp, ",");
+        if (strstr(arg, "color=") == arg) {
+            free(data.colorname);
+            data.colorname = strdup(&arg[6]);
         }
-        free(arguments);
     }
 
-    window = (Window*)malloc(sizeof(Window) * xinfo->screens);
+    free(arguments);
+}
 
-    for (scr = 0; scr < xinfo->screens; scr++) {
+static int module_init(struct aDisplayInfo *dinfo) {
 
-        Colormap colormap = xinfo->colormap[scr];
+    if (!dinfo)
+        return -1;
+
+    Display *dpy = dinfo->display;
+    int scr;
+
+    data.dinfo = dinfo;
+    data.windows = (Window *)malloc(sizeof(Window) * dinfo->screen_nb);
+
+    for (scr = 0; scr < dinfo->screen_nb; scr++) {
+
+        Colormap colormap = dinfo->screens[scr].colormap;
         XSetWindowAttributes xswa;
         XColor color;
 
-        alock_alloc_color(dpy, colormap, color_name, "black", &color);
+        alock_alloc_color(dpy, colormap, data.colorname, "black", &color);
 
         xswa.override_redirect = True;
         xswa.colormap = colormap;
         xswa.background_pixel = color.pixel;
 
-        window[scr] = XCreateWindow(dpy, xinfo->root[scr],
-                0, 0, xinfo->root_width[scr], xinfo->root_height[scr], 0,
+        data.windows[scr] = XCreateWindow(dpy, dinfo->screens[scr].root,
+                0, 0, dinfo->screens[scr].width, dinfo->screens[scr].height, 0,
                 CopyFromParent, InputOutput, CopyFromParent,
                 CWOverrideRedirect | CWColormap | CWBackPixel,
                 &xswa);
 
-        if (window[scr])
-            xinfo->window[scr] = window[scr];
-
     }
 
-    free(color_name);
-    return 1;
+    return 0;
 }
 
-static int alock_bg_blank_deinit(struct aXInfo *xinfo) {
+static void module_free() {
 
-    if (!xinfo || !window)
-        return 0;
+    if (data.windows) {
+        int scr;
+        for (scr = 0; scr < data.dinfo->screen_nb; scr++)
+            XDestroyWindow(data.dinfo->display, data.windows[scr]);
+        free(data.windows);
+        data.windows = NULL;
+    }
 
-    int scr;
-    for (scr = 0; scr < xinfo->screens; scr++)
-        XDestroyWindow(xinfo->display, window[scr]);
-    free(window);
+    free(data.colorname);
+    data.colorname = NULL;
 
-    return 1;
+}
+
+static Window module_getwindow(int screen) {
+    if (!data.windows)
+        return None;
+    return data.windows[screen];
 }
 
 
-struct aBackground alock_bg_blank = {
-    "blank",
-    alock_bg_blank_init,
-    alock_bg_blank_deinit,
+struct aModuleBackground alock_bg_blank = {
+    { "blank",
+        module_loadargs,
+        module_init,
+        module_free,
+    },
+    module_getwindow,
 };

commit ab97db3ddb5c08bb4c4d4c9e113fae53ac0b40e4

Fix hash authentication module
Commit 3ce90ef introduced a bug, which permanently disabled hash
authentication module.

src/auth_hash.c
@@ -208,7 +208,7 @@ static int module_authenticate(const char *pass) {
 
 struct aModuleAuth alock_auth_hash = {
     { "hash",
-        module_dummy_loadargs,
+        module_loadargs,
         module_dummy_loadxrdb,
         module_init,
         module_dummy_free,